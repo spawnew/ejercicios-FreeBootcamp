882aa24457fbf45749a72d5379ee2452613648ae
spawnew <96084099+spawnew@users.noreply.github.com>
2024-07-17 19:09:41 -0300

    terminado

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 let botonazo=document.getElementById('boton')
 let cuerpo=document.getElementById('cuerpo')
-botonazo.addEventListener('click',cambiarfondo =>{
+let texto=document.getElementById('texto')
+const colores = ["Red", "Green", "Blue", "Yellow", "Orange", "Violet","Black"];
+function obtener(){
+    let indice=Math.floor(Math.random() * colores.length)
+return indice;
+}
 
-cuerpo.style.background="black";
+botonazo.addEventListener('click', cambiarfondo =>{
+let elijo=obtener();
+cuerpo.style.background=colores[elijo];
+texto.innerHTML=colores[elijo];
+texto.style.color=colores[elijo];
 
 
 
