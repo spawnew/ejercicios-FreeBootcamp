d465dff1972fd31800ca562355b794e29535b382
spawnew <96084099+spawnew@users.noreply.github.com>
2024-07-17 18:37:44 -0300

    prepando las extensiones

---
+++ app.js
@@ -0,0 +1,9 @@
+let botonazo=document.getElementById('boton')
+let cuerpo=document.getElementById('cuerpo')
+botonazo.addEventListener('click',cambiarfondo =>{
+
+cuerpo.style.background="black";
+
+
+
+})
