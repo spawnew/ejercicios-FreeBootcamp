60bfc0d86dde08097f0a41acb6e443fcfcb3d6f3
spawnew <96084099+spawnew@users.noreply.github.com>
2024-07-18 19:38:04 -0300

    terminada

--- app.js
+++ app.js
@@ -2,19 +2,40 @@
 let  lista=document.getElementById('lista')
 let botonazo2=document.getElementById('botonazo2')
 let input=document.getElementById('input')
+let botonazo3=document.getElementById('botonazo3')
 
-
-
-
-
-
-botonazo2.addEventListener('click',agregar =>{
-     let texto=document.createElement('p');
+input.addEventListener('keydown', (e) => {
+    if (e.key == 'Enter') {
+      agregar();
+    }
+  });
+function agregar(){
+    if(input.value){
+        let texto=document.createElement('p');
      texto.innerHTML=input.value;
      lista.appendChild(texto);
-     input.value="";
-     
-     
+     input.value=""
+     let borrar=document.createElement('i')
+     borrar.classList.add('bi', 'bi-clipboard-minus');
+     borrar.addEventListener('click', ()=>{
+        lista.removeChild(texto);
+     })
+     texto.appendChild(borrar)
+    } 
+    else {
+        alert("no escribiste nada ")
+    }
       
-})
+}
 
+
+
+botonazo2.addEventListener('click',  agregar )
+
+botonazo3.addEventListener('click',() =>{
+       lista.remove();
+
+
+
+
+})
