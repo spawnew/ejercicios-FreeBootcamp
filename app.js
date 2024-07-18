f031680c9daf756e0916bfdb882545a11e05ddce
spawnew <96084099+spawnew@users.noreply.github.com>
2024-07-18 15:12:52 -0300

    empezando ha hacer la lista de tareas

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-let botonazo=document.getElementById('boton')
-let cuerpo=document.getElementById('cuerpo')
-let texto=document.getElementById('texto')
-const colores = ["Red", "Green", "Blue", "Yellow", "Orange", "Violet","Black"];
-function obtener(){
-    let indice=Math.floor(Math.random() * colores.length)
-return indice;
-}
 
-botonazo.addEventListener('click', cambiarfondo =>{
-let elijo=obtener();
-cuerpo.style.background=colores[elijo];
-texto.innerHTML=colores[elijo];
-texto.style.color=colores[elijo];
+let  lista=document.getElementById('lista')
+let botonazo2=document.getElementById('botonazo2')
+let input=document.getElementById('input')
 
 
 
+
+
+
+botonazo2.addEventListener('click',agregar =>{
+     let texto=document.createElement('p');
+     texto.innerHTML=input.value;
+     lista.appendChild(texto);
+     input.value="";
+     
+     
+      
 })
+
